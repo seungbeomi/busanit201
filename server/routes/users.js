0166a772be3979e159b9a038639f1da6a710f45b
seungbeomi <seungbeomi@gmailcom>
2019-10-19 16:09:15 +0900

    node 서버환경구성

---
+++ server/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/auth', function(req, res, next) {
+  
+  const email = req.body.email;
+  const password = req.body.password;
+
+  res.json({ 
+    result : (email == "test@bnksys.co.kr" && password == "1234")
+  });
+});
+
+module.exports = router;
